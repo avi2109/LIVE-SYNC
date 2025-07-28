6bbc8758a14aef618655b11629ec367790abd1b3
avi2109 <40267452+avi2109@users.noreply.github.com>
2025-07-28 22:05:59 +0300

    Add files via upload

---
+++ index.html/sw.js
@@ -0,0 +1,13 @@
+// Service Worker for offline support and PWA functionality
+
+const CACHE_NAME = 'metronome-v1';
+
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.js',
